818e3bc63d257415fa5eea6ef28c4fa57b92f216
Taha Tayyab <125254492+taaaha18@users.noreply.github.com>
2025-09-19 00:54:25 +0500

    Update mails.module.ts

--- src/mails/mails.module.ts
+++ src/mails/mails.module.ts
@@ -7,25 +7,30 @@ import { join } from 'path';
 @Module({
   imports: [
     MailerModule.forRootAsync({
-      useFactory: () => ({
-        transport: {
-          host: 'smtp.gmail.com',
-          port: 587,
-          secure: false,
-          auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS,
+      useFactory: () => {
+        if (!process.env.MAIL_USER || !process.env.MAIL_PASS) {
+          console.error('⚠️ MAIL_USER or MAIL_PASS not set');
+        }
+        return {
+          transport: {
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+              user: process.env.MAIL_USER,
+              pass: process.env.MAIL_PASS,
+            },
           },
-        },
-        defaults: {
-          from: 'Lead Generation "No Reply"',
-        },
-        template: {
-          dir: join(process.cwd(), 'src', 'templates'), //  points to src/templates(bcz by default it points to dist/templates)
-          adapter: new HandlebarsAdapter(),             
-          options: { strict: true },
-        },
-      }),
+          defaults: {
+            from: '"Lead Generation" <no-reply@leadgen.com>',
+          },
+          template: {
+            dir: join(process.cwd(), 'src', 'templates'),
+            adapter: new HandlebarsAdapter(),
+            options: { strict: true },
+          },
+        };
+      },
     }),
   ],
   providers: [MailService],
