76b9c6b39c2de9613a2353a32152e5fc2ec511d4
Taha Tayyab <125254492+taaaha18@users.noreply.github.com>
2025-09-18 20:52:52 +0500

    Update data-source.ts
    supaBase connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,15 @@
 import { DataSource } from 'typeorm';
 import { User } from './users/users.entity';
-import{Job} from './jobs/jobs.entity'
+import { Job } from './jobs/jobs.entity';
 import { Interview } from './interviews/interviews.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '123454321',
-  database: 'lead_generation',
-  entities: [User,Job,Interview],
+  url: process.env.DATABASE_URL, 
+  entities: [User, Job, Interview],
   migrations: ['./migrations/*{.ts,.js}'],
   synchronize: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
