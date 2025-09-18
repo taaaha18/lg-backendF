0937eb85f04036675535a0cef528e8333fb4e5eb
Taha Tayyab <125254492+taaaha18@users.noreply.github.com>
2025-09-18 20:48:16 +0500

    Update app.module.ts
    connection with supabse

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { JobsModule } from './jobs/jobs.module';
 import { InterviewsModule } from './interviews/interviews.module';
 import { MailerModule } from '@nestjs-modules/mailer';
@@ -11,25 +11,22 @@ import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
 import { MailsModule } from './mails/mails.module';
 
-
 @Module({
   imports: [
-    // load .env
     ConfigModule.forRoot({ isGlobal: true }),
 
-    // DB config
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123454321',
-      database: 'lead_generation',
+      url: process.env.DATABASE_URL, 
       autoLoadEntities: true,
       synchronize: false,
       migrationsRun: true,
       migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
+
     UsersModule,
     JobsModule,
     InterviewsModule,
